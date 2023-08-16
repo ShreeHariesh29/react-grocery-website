0911c7273b866de85fdcacdbe333cd93bb347551
ShreeHariesh <shreehariesh29@gmail.com>
2023-08-16 11:17:03 +0530

    components initial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banners from '../Discount components/Banners'
+
+function Home() {
+  return (
+    <div>Home
+        <Banners />
+    </div>
+    
+  )
+}
+
+export default Home
